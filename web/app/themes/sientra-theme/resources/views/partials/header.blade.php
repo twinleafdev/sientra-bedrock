2e05a6ec7cd8dfb1b861d09b28a31652e3c10291
Mike <mikeh@ioprint.com>
2020-05-27 16:02:59 -0400

    insta

--- web/app/themes/sientra-theme/resources/views/partials/header.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <header class="banner">
   <div class="container-fluid">
     <div class="row">
-      <a class="brand col-6 col-lg-12 mr-auto mx-lg-auto my-4 my-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/Sientra-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
+      <a class="brand col-6 col-lg-12 mr-auto mx-lg-auto my-4" href="{{ home_url('/') }}"><img src="@asset('images/Sientra-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
   
-      <nav class="col-6 col-lg-12  ml-auto mx-lg-auto my-4 nav-primary navbar navbar-expand-lg">
+      <nav class="col-6 col-lg-12  ml-auto mx-lg-auto my-4 my-lg-1 nav-primary navbar navbar-expand-lg">
         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar-main-menu" aria-controls="navbar-main-menu" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
