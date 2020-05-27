b43f77e492a8c750264ae67f3a5bf550e8877b2e
Mike <mikeh@ioprint.com>
2020-05-27 19:39:01 -0400

    home

--- web/app/themes/sientra-theme/resources/assets/scripts/routes/common.js
+++ web/app/themes/sientra-theme/resources/assets/scripts/routes/common.js
@@ -5,5 +5,7 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.remove('loading');
   },
 };
--- web/app/themes/sientra-theme/resources/views/layouts/app.blade.php
+++ web/app/themes/sientra-theme/resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
-  <body @php body_class() @endphp>
+  <body @php body_class( 'loading' ) @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
     <div class="wrap container-fluid" role="document">
