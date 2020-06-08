35f4e8e109d2d737f79f113b0740a6f16ba5a3b8
Mike <mikeh@ioprint.com>
2020-06-08 11:44:44 -0400

    augmentation header

--- web/app/themes/sientra-theme/resources/views/partials/header.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/header.blade.php
@@ -1,12 +1,12 @@
 <header class="banner">
-  <img src="@asset('images/locator-form.jpg')" class="locator d-none d-lg-block" alt="locator-form" width="250" height="55" />
+  <img src="@asset('images/locator-tan.svg')" class="locator" alt="locator-form" width="250" height="55" />
   <div class="container-fluid">
-    <div class="row">
-      <a class="brand brand-light col-6 col-lg-12 mx-auto my-4 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/Sientra-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
+    <div class="row ">
+      <a class="brand brand-light col-6 col-lg-12 mx-auto my-2 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/Sientra-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
       
-      <a class="brand brand-dark brand-opus col-6 col-lg-12 mx-auto my-4 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/sientra-opus-logo-w.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
+      <a class="brand brand-dark brand-opus col-6 col-lg-12 mx-auto my-2 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/sientra-opus-logo-w.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
   
-      <nav class="col-6 col-lg-12  ml-auto mx-lg-auto my-4 my-lg-1 nav-primary navbar navbar-expand-lg">
+      <nav class="col-6 col-lg-12  ml-auto mx-lg-auto my-2 my-lg-1 nav-primary navbar navbar-expand-lg">
         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar-main-menu" aria-controls="navbar-main-menu" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
