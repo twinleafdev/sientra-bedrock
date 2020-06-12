5b623b9a9befff8169e6396bd3858c5f967b27a3
Mike <mikeh@ioprint.com>
2020-06-12 11:28:17 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/header.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/header.blade.php
@@ -5,6 +5,9 @@
       <a class="brand brand-light col-6 col-lg-12 mx-auto my-2 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/Sientra-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
       
       <a class="brand brand-dark brand-opus col-6 col-lg-12 mx-auto my-2 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/sientra-opus-logo-w.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
+      
+      <a class="brand brand-light brand-opus col-6 col-lg-12 mx-auto my-2 mb-lg-1" href="{{ home_url('/') }}"><img src="@asset('images/sientra-opus-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
+      
   
       <nav class="col-6 col-lg-12  ml-auto mx-lg-auto my-2 my-lg-1 nav-primary navbar navbar-expand-lg">
         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar-main-menu" aria-controls="navbar-main-menu" aria-expanded="false" aria-label="Toggle navigation">
