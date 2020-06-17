5490d2b9fadf97237f3ff4879f8dd51acbfee057
Mike <mikeh@ioprint.com>
2020-06-17 14:39:58 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implant-basics.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implant-basics.blade.php
@@ -34,7 +34,7 @@
   
       <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>Referenced by some as a “gummy bear” implant, because the highly cohesive silicone gel inside resembles the inside of a gummy bear candy. Cohesive gel implants are available in varying levels of cohesivity which determines the implant’s overall feel.</p> 
         </div>
       </div>
     </div>
@@ -48,7 +48,7 @@
       </div>
       <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>Round breast implants are the most commonly used implant type in the U.S. They offer more lift and fullness overall, especially in the upper portion of the breast. Sientra OPUS round implants are available in either a smooth or textured surface. Teardrop shaped implants offer a more gentle contour in the upper portion and fullness in the lower portion of the breast, below the nipple. Shaped implants are only available in textured surface.</p>
         </div>
       </div>
     </div>
@@ -62,7 +62,7 @@
       </div>
       <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>Textured breast implant shells are not all the same. They vary in their degree of “roughness.” More rough and aggressive shell surfaces have been classified as macrotextured. Sientra’s textured shell has been classified as a microtexture due to its less aggressive, finer surface.5 Sientra’s microtextured surface has been reported to have a lower incidence of BIA-ALCL (cancer of the immune system) than all other U.S. implant brands.<sup>5,6</sup></p>
         </div>
       </div>
     </div>
@@ -76,8 +76,7 @@
       </div>
       <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-        </div>
+<p>The shell of an implant is designed to hold the gel in place. It can either be smooth or textured. Smooth implants are round and can move naturally with the breast tissue. OPUS teardrop shaped implants also move with the breast tissue but feature a microtextured shell surface.5 The less aggressive surface acts as a gentle anchor to keep the implant in place and is designed to minimize rotation. <span class="opus">OPUS</span> implants are shown to have low complication rates.<sup>2</sup></p>        </div>
       </div>
     </div>
     <div class="card">
@@ -90,7 +89,8 @@
       </div>
       <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>The projection of an implant refers to how much it projects forward from the chest wall. The most common implant profiles include low, moderate, moderate plus, high, and Xtra high. 
+          </p>
         </div>
       </div>
     </div>
@@ -104,7 +104,7 @@
       </div>
       <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>The volume of a breast implant is measured in cubic centimeters (cc’s) not in bra cup sizes. The higher the cc, the larger the implant. Together with your board-certified plastic surgeon, you’ll choose which implant size is right for you, proportional and balanced for your body type to deliver the aesthetic look you desire.</p>
         </div>
       </div>
     </div>
@@ -118,7 +118,7 @@
       </div>
       <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>Yes. But when it comes to patient safety, not all implants are created equal. Sientra’s 10-year FDA-required clinical study – the largest breast implant study of its kind – revealed unrivaled safety and clinical results.<sup>2</sup></p>
         </div>
       </div>
     </div>
@@ -132,7 +132,9 @@
       </div>
       <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>You’ll want to choose a surgeon you feel comfortable with and whose work you admire. But you’ll also want to be sure they are board-certified. This ensures they have completed the highest level of training, are most qualified to perform your surgery, and troubleshoot any complications that may arise.</p> 
+<p>Sientra <span class="opus">OPUS</span> is the only silicone gel breast implant brand to sell exclusively to board-certified plastic surgeons. You can trust that only the most trained and most qualified surgeons use our implants.</p> 
+
         </div>
       </div>
     </div>
@@ -146,7 +148,9 @@
       </div>
       <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordionExample">
         <div class="card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          <p>Saline implants are filled with sterile salt water. They are inserted into the body empty and filled once inside. If an implant shell leaks, the implant will collapse and the saline will be absorbed by the body.</p> 
+          <p>Silicone implants are already filled with a highly cohesive gel, prior to insertion. They are designed to mimic the look and feel of natural breast tissue and to maintain their shape over time. If the shell of a silicone implant tears, the cohesive gel is designed to remain inside.</p>
+
         </div>
       </div>
     </div>
