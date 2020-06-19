38d0b2b762e1495b13350aed8fff6f0a4ba8d2b8
Mike <mikeh@ioprint.com>
2020-06-19 18:22:49 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-reconstruction-home.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-reconstruction-home.blade.php
@@ -5,43 +5,111 @@
 
 <section class="intro row" >
   <div class="container">
+    <header class="w-75 mx-auto">
+    	<img src="@asset('images/helping-you-more.svg')" alt="helping-you-more" />
+    </header>
   	<div class="col text-center my-5 light">
   		<p>For many women, restoring the look and feel of their breasts can be emotionally rewarding and promote a general sense of well-being. If you are considering breast reconstruction surgery, Sientra has a comprehensive portfolio of options to support you along your reconstructive journey.</p>
   	</div>  	
   </div><!-- .container -->
 <img src="@asset('images/recon-woman-section-break.jpg')" class="img-fluid" alt="recon-woman-section-break" width="2000" height="759" />
 </section>
-<section class="guide text-center container">
+<section class="guide text-center container light">
+  <header>
+    <img src="@asset('images/breast-surgery-guide.svg')" alt="breast-surgery-guide" />
+  	<h3 class="light">what is breast reconstruction?</h3>
+  </header>
 	<p>Breast reconstruction is a surgical procedure that restores the shape and appearance of a breast. You may consider breast reconstruction following a mastectomy or a lumpectomy.</p>
-	<p>The most common approach is implant-based reconstruction which uses a breast implant to rebuild breast shape.1 </p>
-	<p>Breast reconstruction can either be performed in a one-step procedure, often referred to as direct-to-implant, where the implant is inserted at the time of your mastectomy. Or, more commonly, the reconstruction takes place in two steps.1 </p>
+	<p class="implant-bg">The most common approach is implant-based reconstruction which uses a breast implant to rebuild breast shape.<sup>1</sup> </p>
+	<p>Breast reconstruction can either be performed in a one-step procedure, often referred to as direct-to-implant, where the implant is inserted at the time of your mastectomy. Or, more commonly, the reconstruction takes place in two steps.<sup>1</sup> </p>
 </section>
 <section class="two-step text-center container">
   <div class="inner light">
+    <header>
+    	<img src="@asset('images/two-step-recon.svg')" alt="two-step-recon" />
+    </header>
   	<p>During the <span class="opus">two-step</span> reconstruction method, a temporary breast tissue expander is used to form a new breast pocket which will eventually hold the long-term breast implant.</p>
   </div>
 	
 </section>
-<section class="steps container">
+<section class="steps container light">
 	<div class="row">
-  	<div class="col-2">
-  		STEP ONE
+  	<div class="col-3">
+  		<img src="@asset('images/step-one.svg')" alt="step-one" />
   	</div>
-  	<div class="col-10">
+  	<div class="col-9 d-flex align-items-center">
     	Sterile saline is injected through the expander port to slowly stretch the muscle and skin creating a pocket for the breast implant
   	</div>
   </div>
   <div class="col-12">
-    <img src="@asset('images/expand-steps.jpg')" class="img-fluid" alt="expand-steps" width="1500" height="667" />
+    <img src="@asset('images/expander-diagram.svg')" alt="expander-diagram" />
   </div>
   <div class="row">
-    <div class="col-10 text-right">
+    <div class="col-9 text-right d-flex align-items-center">
       Once the pocket is formed (typically within 2-6 months following surgery) the tissue expander is removed and replaced with a long-term breast implant	
     </div>
-  	<div class="col-2">
-  		STEP TWO
+  	<div class="col-3">
+  		<img src="@asset('images/step-two.svg')" alt="step-two" />
   	</div>
   </div>
+</section>
+<section class="timing boxy-box text-center">
+  <div class="wrap px-0 pl-lg-3">  	
+  	
+  	<div class="col-12 col-lg-5 model text-left px-0 pl-lg-3">
+      <img src="@asset('images/timing-fpo.png')" alt="timing-fpo" width="708" height="834" />
+  	</div>
+
+  	<div class="col-12 col-lg-8 message">
+      <div class="message-inner">
+        <header>
+        	TIMING
+        </header>
+        <div class="row">
+          <div class="col-12">
+            <h3>Immediate vs. Delayed Reconstruction</h3>
+          	<p>Breast reconstruction can begin at the time of your mastectomy (Immediate) or it can be performed months or years later (Delayed). Timing will depend on your stage of cancer and treatment plan post mastectomy:<sup>3</sup></p>
+          </div>
+        
+          <div class="col-6">
+          	Some benefits of
+  Immediate Reconstruction<sup>3</sup>
+            <ul class="text-left">
+              <li>One less surgery and potentially
+              less costs</li>
+              <li>Emotional benefit of beginning reconstruction at same time as 
+              your mastectomy</li>
+            </ul>
+          </div>
+          <div class="col-6">
+          	Some benefits of
+  Delayed Reconstruction<sup>3</sup>
+            <ul class="text-left">
+              <li>Allows for additional cancer therapies (such as chemo and radiation)</li>
+              <li>More time to consider your reconstruction options</li>
+            </ul>
+          </div>  
+          <div class="col-12">
+          	<p>You’ll want to discuss the timing of your breast reconstruction surgery with your doctor. Together you’ll determine your best path forward.</p>
+          </div>      	
+        </div><!-- .row -->
+      </div>
+  	</div>
+  	  	
+  </div>
+  <img src="@asset('images/recon-woman-section-break.jpg')" class="img-fluid" alt="recon-woman-section-break" width="2000" height="759" />
+
+</section>
+
+<section class="options row"> 
+  <header class="col-12">
+  	 <img src="@asset('images/options.svg')" class="img-fluid" alt="options" />
+  </header>
+	<div class="col-12 text-center">
+  	<h2>breast tissue expanders and silicone gel breast implants
+designed for your comfort and peace-of-mind</h2>
+  </div>
+  <img src="@asset('images/options-implant-group.jpg')" class="img-fluid w-75 mx-auto" alt="options-implant-group" width="1800" height="612" />
 </section>
 <?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
 @php the_content() @endphp
