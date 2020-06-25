51c2715c67e02cd20daa8c2fe8fc6245c85a86f2
Mike <mikeh@ioprint.com>
2020-06-25 13:34:56 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-patient-resources.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-patient-resources.blade.php
@@ -16,21 +16,28 @@
 <section class="resources container">
 	<div class="row">
 		<article class="col">
+
+
+
+
+
+
 			<header><h3>Augmentation</h3></header>
-        <span>PDF </span> <p>Sientra Feel Good Factor: Breast Implants That Feel as Amazing as They Look</p>
-        <span>PDF </span> <p>Sientra Patient Planning Guide: Breast Augmentation</p>
-        <span>PDF </span> <p>Sientra Silicone Gel Breast Implants: Quick Facts About Breast Augmentation & Reconstruction</p>
-        <span>PDF </span> <p>Patient Educational Brochure: Breast Augmentation With Sientra Silicone Gel Breast Implants</p>
-        <span>PDF </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Program Information Pamphlet</p>
+        <span><a href="@asset('images/aug/MDC-0351%20R1%20OPUS%20Luxe%20Patient%20Brochure.pdf')" target="_blank">PDF</a> </span> <p>Sientra Feel Good Factor: Breast Implants That Feel as Amazing as They Look</p>
+        <span><a href="@asset('images/aug/Sientra_Patient_Planning_Guide_Breast_Augmentation.pdf')" target="_blank">PDF</a> </span> <p>Sientra Patient Planning Guide: Breast Augmentation</p>
+        <span><a href="@asset('images/aug/Sientra_PatientLabel_Augmentation_MDC-0010%20R2_11.7.17.pdf')" target="_blank">PDF</a> </span> <p>Sientra Silicone Gel Breast Implants: Quick Facts About Breast Augmentation & Reconstruction</p>
+        <span><a href="@asset('images/aug/Sientra_Silicone_Gel_Breast_Implants_Quick_Facts_About_Breast_Augmentation_and_Reconstruction.pdf')" target="_blank">PDF</a> </span> <p>Patient Educational Brochure: Breast Augmentation With Sientra Silicone Gel Breast Implants</p>
+        <span><a href="@asset('images/aug/MDC-0298%20R2%20Platinum20%20Patient%20Leaflet.pdf')" target="_blank">PDF</a> </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Program Information Pamphlet</p>
 		</article>
 		<article class="col">
+  		  		
 			<header><h3>Reconstruction</h3></header>
-        <span>PDF </span> <p>Sientra Feel Good Factor: Breast Implants That Feel as Amazing as They Look</p>
-        <span>PDF </span> <p>Sientra Patient Planning Guide: Breast Reconstruction</p>
-        <span>PDF </span> <p>Sientra Silicone Gel Breast Implants: Quick Facts About Breast Augmentation & Reconstruction</p>
-        <span>PDF </span> <p>Patient Educational Brochure: Breast Reconstruction With Sientra Silicone Gel Breast Implants</p>
-        <span>PDF </span> <p>The Sientra Platinum20<sup>TM</sup> Limited Warranty & Lifetime Product Replacement Program Brochure</p>
-        <span>PDF </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Program Information Pamphlet</p>
+        <span><a href="@asset('images/recon/MDC-0351%20R1%20OPUS%20Luxe%20Patient%20Brochure.pdf')" target="_blank">PDF</a> </span> <p>Sientra Feel Good Factor: Breast Implants That Feel as Amazing as They Look</p>
+        <span><a href="@asset('images/recon/Sientra_Patient_Planning_Guide_Breast_Augmentation.pdf')" target="_blank">PDF</a> </span> <p>Sientra Patient Planning Guide: Breast Reconstruction</p>
+        <span><a href="@asset('images/recon/Sientra_Silicone_Gel_Breast_Implants_Quick_Facts_About_Breast_Augmentation_and_Reconstruction.pdf') " target="_blank">PDF</a> </span> <p>Sientra Silicone Gel Breast Implants: Quick Facts About Breast Augmentation & Reconstruction</p>
+        <span><a href="@asset('images/recon/Sientra_PatientLabel_Augmentation_MDC-0010%20R2_11.7.17.pdf')" target="_blank">PDF</a> </span> <p>Patient Educational Brochure: Breast Reconstruction With Sientra Silicone Gel Breast Implants</p>
+        <span><a href="@asset('images/recon/MDC-0009%20R2%20Warranty.pdf')" target="_blank">PDF</a> </span> <p>The Sientra Platinum20<sup>TM</sup> Limited Warranty & Lifetime Product Replacement Program Brochure</p>
+        <span><a href="@asset('images/recon/MDC-0298%20R2%20Platinum20%20Patient%20Leaflet.pdf')" target="_blank">PDF</a> </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Program Information Pamphlet</p>
 
 		</article>
 	</div><!-- .row -->
@@ -39,16 +46,16 @@
 			<header><h3>Warranty Terms & Conditions</h3></header>
         <p>Please find terms and conditions below by date of implantation:</p>
         <p>April 1, 2012 – Sept 30, 2014</p>
-        <span>PDF </span> <p>Sientra Warranty Terms & Conditions</p>
+        <span><a href="" target="_blank">PDF</a> </span> <p>Sientra Warranty Terms & Conditions</p>
        <p>October 1, 2014 – April 30, 2018</p>
-        <span>PDF </span> <p>Sientra Warranty Terms & Conditions</p>
-        <span>PDF </span> <p>CapCon Care Program Terms & Conditions</p>
+        <span><a href="" target="_blank">PDF</a> </span> <p>Sientra Warranty Terms & Conditions</p>
+        <span><a href="" target="_blank">PDF</a> </span> <p>CapCon Care Program Terms & Conditions</p>
         <p>On or After May 1, 2018</p>
-        <span>PDF </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Terms & Conditions</p>
+        <span><a href="" target="_blank">PDF</a> </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Terms & Conditions</p>
 		</article>
 		<article class="col">
 			<header><h3>Silicone Scar Treatment</h3></header>
-        <span>PDF </span> <p>BIOCORNEUM: See Your Beauty, Not Your Scar</p>
+        <span><a href="" target="_blank">PDF</a> </span> <p>BIOCORNEUM: See Your Beauty, Not Your Scar</p>
 		</article>
 	</div><!-- .row -->
 	<div class="row">
