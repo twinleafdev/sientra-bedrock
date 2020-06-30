7ac406334c307ddbdc632827b55199adbd551e3c
Scott Johnson <scott@asenka.com>
2020-06-30 12:55:13 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-preparing-for-surgery.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-preparing-for-surgery.blade.php
@@ -20,139 +20,133 @@
 	</header>
 	<p class="w-75 mx-auto">There are many decisions to make when selecting the right implant for your body and desired outcome. Below are some initial terms and topics you’ll want to be familiar with going into discussions with your surgeon.</p>
 </section>
-<section class="faq">
-
+<section class="questions">
   <div class="accordion" id="accordionExample">
     <div class="card">
       <div class="card-header" id="headingOne">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            What’s a “highly cohesive” silicone gel implant? 
+            Am I a good candidate for breast reconstruction? 
           </button>
         </h2>
-      </div>
-  
-      <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>Referenced by some as a “gummy bear” implant, because the highly cohesive silicone gel inside resembles the inside of a gummy bear candy. Cohesive gel implants are available in varying levels of cohesivity which determines the implant’s overall feel.</p> 
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingTwo">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Round vs. teardrop shaped implants
+            What type of breast reconstruction procedure is best for me?
           </button>
         </h2>
-      </div>
-      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>Round breast implants are the most commonly used implant type in the U.S. They offer more lift and fullness overall, especially in the upper portion of the breast. Sientra OPUS round implants are available in either a smooth or textured surface. Teardrop shaped implants offer a more gentle contour in the upper portion and fullness in the lower portion of the breast, below the nipple. Shaped implants are only available in textured surface.</p>
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingThree">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            What’s the difference between macrotextured and microtextured implants?
+            What are the primary risks and complications associated with breast reconstruction?
           </button>
         </h2>
-      </div>
-      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>Textured breast implant shells are not all the same. They vary in their degree of “roughness.” More rough and aggressive shell surfaces have been classified as macrotextured. Sientra’s textured shell has been classified as a microtexture due to its less aggressive, finer surface.5 Sientra’s microtextured surface has been reported to have a lower incidence of BIA-ALCL (cancer of the immune system) than all other U.S. implant brands.<sup>5,6</sup></p>
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingFour">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-            Smooth vs. microtextured implants
+            What is the best timing for me? Can I have breast reconstruction during the same surgery as my mastectomy? If not, how long do I need to wait?
           </button>
         </h2>
-      </div>
-      <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
-        <div class="card-body">
-<p>The shell of an implant is designed to hold the gel in place. It can either be smooth or textured. Smooth implants are round and can move naturally with the breast tissue. OPUS teardrop shaped implants also move with the breast tissue but feature a microtextured shell surface.5 The less aggressive surface acts as a gentle anchor to keep the implant in place and is designed to minimize rotation. <span class="opus">OPUS</span> implants are shown to have low complication rates.<sup>2</sup></p>        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingFive">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-            Implant projection
+            Will the reconstructed breast match my remaining breast in size?
           </button>
         </h2>
-      </div>
-      <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>The projection of an implant refers to how much it projects forward from the chest wall. The most common implant profiles include low, moderate, moderate plus, high, and Xtra high. 
-          </p>
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingSix">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-            Implant volume/fullness
+            How will my reconstructed breast feel to the touch?
           </button>
         </h2>
-      </div>
-      <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>The volume of a breast implant is measured in cubic centimeters (cc’s) not in bra cup sizes. The higher the cc, the larger the implant. Together with your board-certified plastic surgeon, you’ll choose which implant size is right for you, proportional and balanced for your body type to deliver the aesthetic look you desire.</p>
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingSeven">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
-            Are silicone gel breast implants safe?
+            Will I have any feeling in my reconstructed breast?
           </button>
         </h2>
-      </div>
-      <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>Yes. But when it comes to patient safety, not all implants are created equal. Sientra’s 10-year FDA-required clinical study – the largest breast implant study of its kind – revealed unrivaled safety and clinical results.<sup>2</sup></p>
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingEight">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
-            Choosing your surgeon
+            Are you board certified (or board eligible) by The American Board of Plastic Surgery (ABPS)? (You can research board membership at www.abplsurg.org) 
           </button>
         </h2>
-      </div>
-      <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>You’ll want to choose a surgeon you feel comfortable with and whose work you admire. But you’ll also want to be sure they are board-certified. This ensures they have completed the highest level of training, are most qualified to perform your surgery, and troubleshoot any complications that may arise.</p> 
-<p>Sientra <span class="opus">OPUS</span> is the only silicone gel breast implant brand to sell exclusively to board-certified plastic surgeons. You can trust that only the most trained and most qualified surgeons use our implants.</p> 
-
-        </div>
       </div>
     </div>
     <div class="card">
       <div class="card-header" id="headingNine">
         <h2 class="mb-0">
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
-            What’s the difference between saline & silicone?
+            How many breast reconstruction surgeries do you perform each year?
           </button>
         </h2>
-      </div>
-      <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordionExample">
-        <div class="card-body">
-          <p>Saline implants are filled with sterile salt water. They are inserted into the body empty and filled once inside. If an implant shell leaks, the implant will collapse and the saline will be absorbed by the body.</p> 
-          <p>Silicone implants are already filled with a highly cohesive gel, prior to insertion. They are designed to mimic the look and feel of natural breast tissue and to maintain their shape over time. If the shell of a silicone implant tears, the cohesive gel is designed to remain inside.</p>
-
-        </div>
-      </div>
+      </div> 
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingNine">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+            How many years have you been doing breast reconstruction surgery? 
+          </button>
+        </h2>
+      </div> 
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingNine">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+            Which types of reconstruction are you most experienced in?
+          </button>
+        </h2>
+      </div> 
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingNine">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+            What is the most common complication you encounter with breast reconstruction patients? 
+          </button>
+        </h2>
+      </div> 
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingNine">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+            What is your reoperation rate for reconstruction patients? And what is the most common reoperation you perform?
+          </button>
+        </h2>
+      </div> 
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingNine">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+            Do you have before-and-after photos of similar procedures that I can review? 
+          </button>
+        </h2>
+      </div> 
     </div>
   
   </div>
