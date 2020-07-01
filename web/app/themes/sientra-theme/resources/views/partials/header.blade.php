f8464bc1536edc968fe808ed83da369ddd740a0f
Mike <mikeh@ioprint.com>
2020-07-01 16:39:56 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/header.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/header.blade.php
@@ -8,6 +8,8 @@
       
       <a class="brand brand-light brand-opus col-6 col-lg-12 mx-auto my-lg-3" href="{{ home_url('/') }}"><img src="@asset('images/sientra-opus-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
       
+      <a class="brand brand-recon col-6 col-lg-12 mx-auto my-lg-3" href="{{ home_url('/') }}"><img src="@asset('images/sientra-opus-recon-logo.svg')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
+      
   
       <nav class="col-6 col-lg-12  ml-auto mx-lg-auto my-2 mt-lg-2 mb-lg-1 nav-primary navbar navbar-expand-lg">
         <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar-main-menu" aria-controls="navbar-main-menu" aria-expanded="false" aria-label="Toggle navigation">
