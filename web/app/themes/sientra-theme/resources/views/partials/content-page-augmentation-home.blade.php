60207f7a87f6e4a1d3004710fff265ffac165677
Mike <mikeh@ioprint.com>
2020-07-02 10:15:18 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-augmentation-home.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-augmentation-home.blade.php
@@ -142,6 +142,26 @@ with my results <span class="q">&#8221;</span><br><span class="sig">- RealSelf M
 	</div>
 </section>
 
+<video
+    id="my-player"
+    class="video-js"
+    controls
+    preload="auto"
+    poster="//vjs.zencdn.net/v/oceans.png"
+    data-setup='{}'>
+  <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
+  <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm"></source>
+  <source src="//vjs.zencdn.net/v/oceans.ogv" type="video/ogg"></source>
+  <p class="vjs-no-js">
+    To view this video please enable JavaScript, and consider upgrading to a
+    web browser that
+    <a href="https://videojs.com/html5-video-support/" target="_blank">
+      supports HTML5 video
+    </a>
+  </p>
+</video>
+
+
 <section class="row realself text-center">
   <div class="container-fluid wrap px-0 px-lg-3">  	
   	
