91e5d22ec703c4c1261d45fbd1dc51af0f69044a
Mike <mikeh@ioprint.com>
2020-07-06 17:17:10 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-commitment-to-safety.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-commitment-to-safety.blade.php
@@ -2,6 +2,144 @@
 	
 </section>
 
+<section class="lead my-5">
+	<h2>We’re guided by the science supporting our products which continues to confirm the long-term safety and effectiveness of all Sientra implants.</h2>
+<p>At Sientra, patient safety and product quality are our highest priority. That is why, from our inception, we have exclusively entrusted the safety and patient satisfaction of our breast implants to only the most qualified surgeons in our industry - board-certified and board-eligible plastic surgeons. Sientra is committed to providing transparent, evidence-based information about our implants to help physicians and patients make an informed decision.</p>
+</section>
+
+<section class="faq">
+
+    <div class="card">  
+      <div class="card-body light">
+        <h2>Understand the Benefits and Risks</h2>
+        <p>Undergoing any surgical procedure involves risks. We entrust patient safety and satisfaction to only the most qualified surgeons in our industry, which is why we offer OPUS breast implants exclusively to board-certified or board-eligible plastic surgeons in the United States. Women should always talk to their board-certified or board-eligible plastic surgeon about the different implant options, the known benefits and risks associated with any procedure, and their individual needs, to make an informed decision. It is important for women to know that complications may occur after surgery with breast implants. Below are a few of the key complications associated with breast implant surgery reported in Sientra’s 10-Year Clinical Study.<sup>1</sup></p>
+      </div>
+    </div>
+
+    <div class="card">  
+      <div class="card-body light">
+        <h2>Implant Rupture</h2>
+        <p>Through 10 Years, in general, a patient’s risk of developing a rupture is 7.9%. This means 8 out 100 patients may experience a rupture through 10 years.<sup>*</sup></p>
+      </div>
+    </div>
+    
+    <div class="card">  
+      <div class="card-body light">
+        <h2>Capsular Contracture</h2>
+        <p>During the healing process after surgery, in some women, the scar tissue (capsule) squeezes the implant. It can result in firmness or hardening of the breast. Through 10 Years, in general, a patient’s risk of capsular contracture is 13.5%.<sup>1</sup> This means 14 out of 100 patients may experience capsular contracture within 10 years after receiving implants.<sup>*</sup></p>
+      </div>
+    </div>
+
+    <div class="card">  
+      <div class="card-body light">
+        <h2>Breast Implant-Associated Anaplastic Large Cell Lymphoma (BIA-ALCL)</h2>
+        <p> BIA-ALCL is NOT breast cancer. BIA-ALCL is a rare and treatable type of non-Hodgkin T-cell lymphoma. Women with breast implants have a small but increased risk of developing this.<sup>2,3</sup></p>
+        <p>Current data suggests that BIA-ALCL occurs more frequently in women with textured implants than smooth implants.<sup>4</sup> BIA-ALCL has been diagnosed in women with silicone and saline implants.<sup>5</sup> Worldwide, the reported incidence rates of BIA-ALCL vary widely and within different implant surfaces.6 When caught early, BIA-ALCL is usually curable.<sup>7</sup></p>
+        <p>It is important to understand how rare the occurrence is and put the numbers into perspective. The risk of developing ALCL from a breast implant is 0.003%.<sup>8</sup>
+        It is important for women to be advocates for their breast health and be aware of any changes in size or shape of their breasts or breast implants. For any woman who is concerned about their breast health, they should see their board-certified or board-eligible plastic surgeon for evaluation.<sup>7</sup></p>
+        <p>Learn more about potential breast implant risk in augmentation and reconstruction</p>
+      </div>
+    </div>
+
+    <div class="card">  
+      <div class="card-body light">
+        <h2>Things to Remember:</h2>
+        <p>Long-term clinical and real-world data supports the safety of breast implants.<sup>9</sup> Because of this:</p>
+        <ul>
+          <li>- The FDA and all leading medical societies currently do not recommend the removal of implants in women without symptoms.</li>
+          <li>- The FDA and leading medical societies currently do not recommend changing the routine medical care and follow-up for women with implants. Patients should contact their board-certified plastic surgeon with any changes to breast shape or size, including pain, lumps or swelling.<sup>4</sup></li>
+        </ul>
+      </div>
+    </div>
+    <div class="card">  
+      <div class="card-body light">
+        <h2>Sientra OPUS breast implants are FDA-approved and safe devices that offer many quality of life benefits for women. </h2>
+        <p>Women choose primary breast augmentation surgery to increase the size and proportion of their breast(s). Most women who have undergone breast implant surgery have reported high levels of satisfaction with their body image and the shape, feel and size of their implants. In Sientra’s Clinical Study, most primary and revision-augmentation patients were pleased with the results of their implant surgery. Most women who underwent primary or revision-augmentation with Sientra Implants felt their breast implants made them feel more feminine and more attractive. In addition, the majority of these women indicated that their breast implants made them feel better about themselves. </p>
+        
+      <div class="container mt-5">
+        <div class="row text-center"> 
+      	  
+        <!-- 	 -->
+        	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+          	<div class="rounded-circle _90">
+          		<strong>90%</strong><br>
+          	</div>
+        		<h3>Felt more feminine</h3>
+        	</article>
+        <!-- 	 -->
+        	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+          	<div class="rounded-circle _91">
+          		<strong>91%</strong><br>
+          	</div>
+        		<h3>Felt their breasts look natural and soft</h3>
+        	</article>
+        <!-- 	 -->
+        	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+          	<div class="rounded-circle _79">
+          		<strong>79%</strong><br>
+          	</div>
+        		<h3>Felt their clothes
+    fit better</h3>
+        	</article>
+        <!-- 	 -->
+          <article class="col-12 mt-4">
+          	    *(primary augmentation cohort)
+          </article>
+        </div><!-- .row -->
+      </div><!-- .container -->
+
+        
+      </div>
+    </div>    
+  
+</section>
+<small><sup>*</sup>Average rate of specific complication reported across all study cohorts. Individual rates may be higher or lower depending upon multiple factors for each patient.</small> 
+
+<section class="warranty container mt-5">
+  <div class="row">
+    <div class="col-3">
+  		<img src="@asset('images/warranty.svg')" alt="warranty" />
+  	</div>
+  	<div class="col-8">
+  		<h3 class="medium opus">First Company to Provide 20-Year Warranty</h3>
+      <p class="light">Because Sientra trusts in the safety and performance of our breast implants, we offer the Platinum20 Warranty, the most complete warranty program, and we are the first company to provide 20-year coverage. <a href="#">learn more here.</a></p>
+  	</div>
+  	<div class="col-12 text-center my-5">
+  		<p>If you’re ready to explore your options regarding breast augmentation or reconstruction surgery, 
+we encourage you to reach out to your board-certified plastic surgeon. </p>
+  	</div>
+  </div>
+</section> 
+
+<section class="resources container">
+	<div class="row">
+		<article class="col">
+			<header><h3 class="image-h my-4" style="background-image:url(@asset('images/for-physicians.svg'));  height: 4.0rem; margin-top: 0.5rem; ">For Physicians</h3></header>
+        <span><a href="@asset('images/aug/MDC-0351_R1_OPUS_Luxe_Patient_Brochure.pdf')" target="_blank">PDF</a> </span> <p>Sientra Feel Good Factor: Breast Implants That Feel as Amazing as They Look</p>
+        <span><a href="@asset('images/aug/Sientra_Patient_Planning_Guide_Breast_Augmentation.pdf')" target="_blank">PDF</a> </span> <p>Sientra Patient Planning Guide: Breast Augmentation</p>
+        <span><a href="@asset('images/aug/Sientra_PatientLabel_Augmentation_MDC-0010_R2_11-7-17.pdf')" target="_blank">PDF</a> </span> <p>Sientra Silicone Gel Breast Implants: Quick Facts About Breast Augmentation & Reconstruction</p>
+        <span><a href="@asset('images/aug/Sientra_Silicone_Gel_Breast_Implants_Quick_Facts_About_Breast_Augmentation_and_Reconstruction.pdf')" target="_blank">PDF</a> </span> <p>Patient Educational Brochure: Breast Augmentation With Sientra Silicone Gel Breast Implants</p>
+        <span><a href="@asset('images/aug/MDC-0298_R2_Platinum20_Patient_Leaflet.pdf')" target="_blank">PDF</a> </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Program Information Pamphlet</p>
+		</article>
+		
+		<article class="col patient">
+  		  		
+			<header><h3 class="image-h my-4" style="background-image:url(@asset('images/for-your-patients.svg')); height: 4.5rem; ">For Your Patients</h3></header>
+        <span><a href="@asset('images/recon/MDC-0351_R1_OPUS_Luxe_Patient_Brochure.pdf')" target="_blank">PDF</a> </span> <p>Sientra Feel Good Factor: Breast Implants That Feel as Amazing as They Look</p>
+        <span><a href="@asset('images/recon/Sientra_Patient_Planning_Guide_Breast_Augmentation.pdf')" target="_blank">PDF</a> </span> <p>Sientra Patient Planning Guide: Breast Reconstruction</p>
+        <span><a href="@asset('images/recon/Sientra_Silicone_Gel_Breast_Implants_Quick_Facts_About_Breast_Augmentation_and_Reconstruction.pdf') " target="_blank">PDF</a> </span> <p>Sientra Silicone Gel Breast Implants: Quick Facts About Breast Augmentation & Reconstruction</p>
+        <span><a href="@asset('images/recon/Sientra_PatientLabel_Augmentation_MDC-0010_R2_11-7-17.pdf')" target="_blank">PDF</a> </span> <p>Patient Educational Brochure: Breast Reconstruction With Sientra Silicone Gel Breast Implants</p>
+        <span><a href="@asset('images/recon/MDC-0009_R2_Warranty.pdf')" target="_blank">PDF</a> </span> <p>The Sientra Platinum20<sup>TM</sup> Limited Warranty & Lifetime Product Replacement Program Brochure</p>
+        <span><a href="@asset('images/recon/MDC-0298_R2_Platinum20_Patient_Leaflet.pdf')" target="_blank">PDF</a> </span> <p>Sientra Platinum20<sup>TM</sup> Warranty Program Information Pamphlet</p>
+
+		</article>
+	
+	</div><!-- .row -->	
+	
+</section>
+
+
+
 
 <?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
-@php the_content() @endphp
+<!-- @php the_content() @endphp -->
