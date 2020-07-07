588df4e37a062994684c935a10f60a70c6774a3a
Mike <mikeh@ioprint.com>
2020-07-07 16:34:37 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-for-us-surgeons-home.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-for-us-surgeons-home.blade.php
@@ -1,3 +1,131 @@
-content-page-for-us-surgeons.blade.php
+<section class="hero row">
+	<img src="@asset('images/surg-home-hero.jpg')" class="img-fluid" alt="surg-home-hero" width="2500" height="1224" />
+</section>
+
+<section class="row why-choose text-center my-4  py-5">
+	<header class="col-12 mb-2 mb-sm-5">
+  	<img src="@asset('images/why-choose.svg')" alt="why choose" />
+		<h3>Sientra <span class="opus">OPUS</span> implants?</h3>
+	</header> 
+	<article class="col-12 col-sm pt-3 pt-sm-0">
+		<p class="lead">Next-generation <br>
+implant science</p>
+<span class="opus">•••</span><br>
+Implants designed <br>
+to perform better with a high safety profile.<sup>1,2</sup>
+	</article>
+	<hr class="w-50 d-block d-sm-none">
+	<article class="col-12 col-sm pt-sm-0">
+		<p class="lead">Only the best, most<br>  
+highly trained plastic surgeons</p>
+<span class="opus">•••</span><br>
+Sientra is available exclusively through board-certified plastic surgeons. With <span class="opus">OPUS</span> you can trust you are in 
+exceptionally skilled hands. 
+	</article>
+	<hr class="w-50 d-block d-sm-none">
+	<article class="col-12 col-sm pt-sm-0">
+		<p class="lead">The industry’s most comprehensive
+20-year warranty</p>
+<span class="opus">•••</span><br>
+It’s more than a warranty, it’s 
+our commitment to your health and safety. 
+	</article>
+</section>
+
+<section class="expanders row my-5">
+	<div class="col-12 text-center">
+		<h2 class="light"><span class="opus">breast implants</span> and <span class="opus">breast tissue expanders</span> uniquely designed to improve patient outcomes</h2>
+	</div>
+	<div class="allo-derm container">
+<img src="@asset('images/dermasphere.jpg')" alt="dermasphere" width="1831" height="1500" /><img src="@asset('images/allox2.jpg')" alt="allox2" width="1843" height="1500" />
+		
+	</div>
+</section>
+
+<section class="patient-safety-break row my-5">
+	<div class="col-6 light text-center">When it comes to patient safety not all implants are created equal. Sientra OPUS implants are clinically shown to have low complication rates.<sup>1</sup></div>
+</section>
+
+<section class="pair row text-center py-5 mt-md-5">
+
+	<div class="col-12 py-4 mt-md-5 tag">
+		<h3><span><strong>1,116</strong> patients enrolled</span><span>evaluated for <strong>10 years</strong></span></h3>
+	</div>
+
+  <div class="container my-5">
+    <div class="row">    
+    <!-- 	 -->
+    	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+      	<div class="rounded-circle">
+      		<strong>Low</strong>
+    <p>capsular contracture rate*</p>
+      	</div>
+    	</article>
+    <!-- 	 -->
+    	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+      	<div class="rounded-circle">
+      		<strong>Lowest</strong>
+      		<p>rupture rate*</p>
+    		</div> 
+    
+    	</article>
+    <!-- 	 -->
+    	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+      	<div class="rounded-circle">
+      		<strong>Lowest</strong>
+      		<p>reoperation rate*</p>
+      	</div>
+ 
+    	</article>
+    <!-- 	 -->
+      <article class="col-12 mt-5">
+      	    <p>*(primary augmentation cohort)</p>
+      </article>
+    </div><!-- .row -->
+  </div><!-- .container -->
+
+</section>
+
+<section class="factor row text-center py-5">
+  <div class="container">
+    <div class="row">
+    	<div class="col-12 py-4 tag">
+      	<img src="@asset('images/feel-good-factor.svg')" alt="feel good factor" />
+    		<h2>patient satisfaction rates<sup>2</sup></h2>
+    	</div>  
+  	  
+    <!-- 	 -->
+    	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+      	<div class="rounded-circle _90">
+      		<strong>90%</strong><br>
+      	</div>
+    		<h3>Felt more feminine</h3>
+    	</article>
+    <!-- 	 -->
+    	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+      	<div class="rounded-circle _91">
+      		<strong>91%</strong><br>
+      	</div>
+    		<h3>Felt their breasts look natural and soft
+</h3>
+    	</article>
+    <!-- 	 -->
+    	<article class="col-12 col-sm-4 mb-5 mb-sm-0">
+      	<div class="rounded-circle _79">
+      		<strong>79%</strong><br>
+      	</div>
+    		<h3>Felt their clothes
+fit better</h3>
+    	</article>
+    <!-- 	 -->
+      <article class="col-12 mt-4">
+      	    *(primary augmentation cohort)
+      </article>
+    </div><!-- .row -->
+  </div><!-- .container -->
+
+</section>
+
+
 <?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
 @php the_content() @endphp
