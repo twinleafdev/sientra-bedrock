7f1e80f44c7dabb83c647e90bb9af4b75480f7c4
Mike <mikeh@ioprint.com>
2020-07-07 17:51:00 -0400

    pages

---
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implants.blade.php
@@ -0,0 +1,72 @@
+<section class="science boxy-box text-center">
+  <div class="wrap px-0 pl-lg-3">  	
+  	
+  	<div class="col-12 col-lg-8 model text-left px-0 pl-lg-3">
+      <img src="@asset('images/implant-science.jpg')" alt="pittman" width="1200" height="691" />
+  	</div>
+
+  	<div class="col-12 col-lg-5 message bg-white">
+      <div class="message-inner">
+         <img src="@asset('images/advanced-science.svg')" class="mx-auto" alt="advanced-science" />
+      </div>
+  	</div>
+  	  	
+  </div>
+</section>
+
+<section class="desired-look text-center mx-auto">
+  <header>
+    <img src="@asset('images/desired-look.svg')" class="img-fluid" alt="achieve your desired look" />
+  </header>
+	<div class="h2-wrap">
+		<h2>design features unique to all OPUS implants</h2>
+	</div>
+	<div class="row">
+		<div class="col-12 col-md-6 my-3 px-2">
+			<img src="@asset('images/desires-diagram-round.jpg')" class="img-fluid d-md-none" alt="desires-diagram-round" width="1000" height="597" />
+		</div>
+		<div class="col-12 col-md-6 my-3 px-2">
+			<img src="@asset('images/desires-diagram-teardrop.jpg')" class="img-fluid d-md-none" alt="desires-diagram-teardrop" width="1091" height="845" />
+		</div>
+	</div><!-- .row -->
+	<img src="@asset('images/desires-diagram.jpg')" class="img-fluid my-5 d-none d-md-block" alt="desires-diagram" width="2500" height="911" />
+</section>
+<section class="surfaces row text-center">
+	<div class="mx-3 mb-3 wrap">
+  	<div>
+    	<img src="@asset('images/smooth-surfaces.svg')" alt="smooth-surfaces" />
+  		<p>Are soft yet durable. Smooth shells keep the gel inside and allow the implant to move naturally with the breast tissue.</p>
+  	</div>
+	</div>
+	<div class="mx-3 mb-3 wrap">
+  	<div>
+	  	<img src="@asset('images/microtextured-surfaces.svg')" class="micro" alt="textured-surfaces" />
+<p>More rough and aggressive shell surfaces have been classified as macrotextured. Sientra’s textured shell has been classified as a microtexture due to its less aggressive, finer surface.<sup>5</sup> Sientra’s <span class="opus">microtextured</span> surface has been reported to have a lower incidence
+of BIA-ALCL (cancer of the immune system) than all other U.S. implant brands.<sup>5,6</sup></p>
+  	</div>
+	</div>
+</section>
+
+
+
+
+
+<section class="surfaces row text-center">
+	<div class="mx-3 mb-3 wrap">
+  	<div>
+    	<img src="@asset('images/smooth-surfaces.svg')" alt="smooth-surfaces" />
+  		<p>Are soft yet durable. Smooth shells keep the gel inside and allow the implant to move naturally with the breast tissue.</p>
+  	</div>
+	</div>
+	<div class="mx-3 mb-3 wrap">
+  	<div>
+	  	<img src="@asset('images/microtextured-surfaces.svg')" class="micro" alt="textured-surfaces" />
+<p>More rough and aggressive shell surfaces have been classified as macrotextured. Sientra’s textured shell has been classified as a microtexture due to its less aggressive, finer surface.<sup>5</sup> Sientra’s <span class="opus">microtextured</span> surface has been reported to have a lower incidence
+of BIA-ALCL (cancer of the immune system) than all other U.S. implant brands.<sup>5,6</sup></p>
+  	</div>
+	</div>
+</section>
+
+
+<?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
+@php the_content() @endphp
