2d20913e502fd27b4980ecd0e44ff49e3b210b77
Mike <mikeh@ioprint.com>
2020-07-08 12:28:02 -0400

    pages

---
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-breast-tissue-expanders.blade.php
@@ -0,0 +1,16 @@
+<section class="boxy-box text-center">
+  <div class="wrap px-0 pl-lg-3">  	
+  	
+  	<div class="col-12 col-lg-8 model text-left px-0 pl-lg-3">
+      <img src="@asset('images/expander-implant-options.jpg')" alt="expander-implant-options" width="1800" height="1038" />
+  	</div>
+  	<div class="col-12 col-lg-5 message bg-white">
+      <div class="message-inner">
+        <img src="@asset('images/expander-implant-options.svg')" alt="expander-implant-options" />
+      </div>
+  	</div>
+  	  	
+  </div>
+</section>
+<?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
+@php the_content() @endphp
