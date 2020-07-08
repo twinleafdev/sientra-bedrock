287486c7ce9f943e464d62c1cf302cb67eabdf23
Mike <mikeh@ioprint.com>
2020-07-08 12:09:01 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implants.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implants.blade.php
@@ -70,7 +70,159 @@ of BIA-ALCL (cancer of the immune system) than all other U.S. implant brands.<su
   	</div>
 	</div>
 </section>
+<section class="strength row">
+	<header>
+		<img src="@asset('images/strength-softness.svg')" alt="strength-softness" />
+	</header>
+	<div class="col-6">
+		<img src="@asset('images/gel-graph.svg')" alt="gel-graph" />
+	</div>
+	<div class="col-6 align-self-center">
+		OPUS HSC & HSC+ implants show minimum trade-off in softness in order to achieve greater gel strength<sup>2</sup>
+	</div>
+</section>
+<section class="luxe row mb-5">
+ <header class="col-6 col-sm-4"> <img src="@asset('images/opus-luxe.png')" class="img-fluid" alt="opus-luxe" width="949" height="353" /></header>
+ <div class="_250-round col-12 col-md-4 mb-2">
+  <img src="@asset('images/250-round.svg')" alt="250-round" />
+ </div>
+ 
+ 
+ <div class="d-none d-md-block luxe-projections text-center">
+ 	<div class="xtra-high projection">
+		
+ 	</div>
+ 	<div class="high projection">
+ 		
+ 	</div> 
+ 	<div class="mod-plus projection">
+		
+ 	</div>
+ 	<div class="mod projection">
+ 		
+ 	</div> 
+ 	<div class="low projection">
+ 		
+ 	</div> 
+ 	
+ 	<div class="xtra-high projection-bg">
+ 		<p><span class="opus">Xtra high</span> projection  <span class="opus">|</span>  (275 cc - 510 cc)</p>
+ 	</div>
+ 	<div class="high projection-bg">
+ 		<p><span class="opus">high</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+ 	</div>
+ 	<div class="mod-plus projection-bg">
+ 		<p><span class="opus">moderate plus</span> projection  <span class="opus">|</span>  (175 cc - 695 cc)</p>
+ 	</div>
+ 	<div class="mod projection-bg">
+ 		<p><span class="opus">moderate</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+ 	</div>
+ 	<div class="low projection-bg">
+ 		<p><span class="opus">low</span> projection  <span class="opus">|</span>  (160 cc - 700 cc)</p>
+ 	</div>
+ </div>
+ 
+ 
+ <div class="implant col-12 col-md-4 text-center d-md-none">
+   
+     <div class="swiper-container swiper-luxe-projection">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="@asset('images/luxe-projection-low.png')" class="img-fluid" alt="luxe-projection-low" width="800" height="679" />
+          <p><span class="opus">low</span> projection  <span class="opus">|</span>  (160 cc - 700 cc)</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="@asset('images/luxe-projection-moderate.png')" class="img-fluid" alt="luxe-projection-moderate" width="800" height="554" />
+          <p><span class="opus">moderate</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="@asset('images/luxe-projection-moderate-plus.png')" class="img-fluid" alt="luxe-projection-moderate-plus" width="800" height="570" />
+          <p><span class="opus">moderate plus</span> projection  <span class="opus">|</span>  (175 cc - 695 cc)</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="@asset('images/luxe-projection-high.png')" class="img-fluid" alt="luxe-projection-high" width="800" height="592" />       
+          <p><span class="opus">high</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="@asset('images/luxe-projection-xrta-high.png')" class="img-fluid" alt="luxe-projection-xrta-high" width="800" height="506" />
+          <p><span class="opus">Xtra high</span> projection  <span class="opus">|</span>  (275 cc - 510 cc)</p>
+        </div>
+      </div><!-- .swiper-wrapper -->
+     </div>
+
+ 
+ </div>
+</section>
+<section class="curve row mb-5">
+ <div class="_90-options col-12 col-md-4 mb-2 offset-md-5">
+  <img src="@asset('images/90-options.svg')" alt="90-options" />
+ </div>
+ 
+  <div class="d-none d-md-flex col-md-3 curve-projections text-center">
+
+  	<div class="high projection">
+  		
+  	</div> 
+
+  	<div class="low projection">
+  		
+  	</div> 
+  	
+  	<div class="high projection-bg">
+  		<p><span class="opus">high</span> projection  <span class="opus">|</span>  (190 cc - 635 cc)</p>
+  	</div>
+
+  	<div class="low projection-bg">
+  		<p><span class="opus">moderate</span> projection  <span class="opus">|</span>  (160 cc - 700 cc)</p>
+  	</div>
+ </div>
+ 
+ <div class="implant col-12 d-md-none text-center">
+   
+     <div class="swiper-container swiper-curve-projection">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+           <img src="@asset('images/curve-projection-low.png')" class="img-fluid" alt="curve-projection-low" width="500" height="1048" />
+           <p><span class="opus">moderate</span> projection  <span class="opus">|</span>  (160 cc - 700 cc)</p>
+        </div>
+        <div class="swiper-slide">
+          <img src="@asset('images/curve-projection-high.png')" class="img-fluid" alt="curve-projection-high" width="500" height="1046" />
+          <p><span class="opus">high</span> projection  <span class="opus">|</span>  (190 cc - 635 cc)</p>
+        </div>
+      </div><!-- .swiper-wrapper -->
+     </div>
+ 	
+ </div>
+
+</section>
+<section class="love my-5">
+	<div class="col-12 col-md-5">
+		<img src="@asset('images/woman-love-results.svg')" alt="woman love the results" />
+	</div>
+	<div class="col-12 col-md-8 testimonial ml-auto">
+  	<img src="@asset('images/quote.svg')" class="quote-icon" alt="quote" />
+  	<p><span class="q">&#8220;</span> I went to the beach over the weekend and it’s the first time in a while I wore a bikini! I couldn’t be happier
+with my results <span class="q">&#8221;</span><br><span class="sig">- RealSelf Member</span></p>
+	</div>
+</section>
 
 
+<section class="row realself text-center">
+  <div class="container-fluid wrap px-0 px-lg-3">  	
+  	
+  	<div class="col-12 col-lg-8 model text-left px-0 px-lg-3">
+      <img src="@asset('images/realself-model.jpg')" alt="pittman" width="800" height="767" />
+  	</div>
+
+  	<div class="col-12 col-lg-5 ml-auto rated">
+      <div class="rated-inner">
+        <img src="@asset('images/realself.svg')" alt="realself" />
+      	<h2>The <span class="opus"><strong>highest rated</strong></span><br>breast implant brand in the U.S.*</h2>
+      </div>
+      <small>*Among silicone gel breast implants; As of March, 2020, realself.com</small>
+  	</div>
+  	  	
+  </div>
+</section>
 <?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
 @php the_content() @endphp
