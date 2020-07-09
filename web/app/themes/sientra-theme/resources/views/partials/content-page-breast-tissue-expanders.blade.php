c988b72293947fadeebd316ff96ca41bfd14e3e4
Mike <mikeh@ioprint.com>
2020-07-09 17:08:39 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-breast-tissue-expanders.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-breast-tissue-expanders.blade.php
@@ -12,5 +12,49 @@
   	  	
   </div>
 </section>
+<section class="expander-options text-center">
+	<header class="col-12">
+		<h2>Breast Tissue Expander Options</h2>
+	</header>
+	<div class="wrap row">
+  	<div class="col-6 offset-6">
+  		<h2 class="black">AlloX2<sup>®</sup></h2>
+  		<h3>Revolutionary dual-port design and integral drain allow for less invasive – non-surgical – draining of serous fluid</h3>
+      <p>•  Designed to mitigate key risks associated with breast expansion and ultimately reduce reoperation rates</p> 
+      <p>•  Only tissue expander with an integral drain providing direct access to the periprosthetic space where fluid can accumulate</p>
+      <p>•  Allows for diagnostic fluid sampling to enable a faster treatment response</p>
+  	</div>
+  	<div class="allox-info-box col-5 offset-7">
+  		<img src="@asset('images/allox2-info-box.jpg')" class="img-fluid" alt="allox2-info-box" width="750" height="409" />
+  	</div>
+	</div>
+</section>
+<section class="row" id="testimonials">
+  <img src="@asset('images/quote.svg')" class="quote-icon" alt="quote" />
+<!-- Slider main container -->
+  <div class="swiper-container swiper-testimonial">
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide"><p><span class="q">&#8220;</span>The unique feature of the AlloX2 provides surgeons easy access to the periprosthetic space without altering any of the other characteristics of a tissue expander. While one does not plan to experience a postoperative complication, a valid question in light of the availability of the AlloX2 is why one would forego utilizing a device that facilitates treatment thereof.<span class="q">&#8221;</span><br><span class="sig">Arash Momeni, MD<br>Board-Certified Plastic and Reconstructive Surgeon<br>Palo Alto Medical Center</span></p></div>
+  
+      </div>  
+  
+  </div>
+</section>
+<section class="dermaspan row text-center">
+	<div class="col-6">
+		<h2 class="black">Dermaspan<sup>TM</sup></h2>
+		<h3>Soft, refined design allows for <span class="opus">gentle</span> and <span class="opus">more comfortable</span> expansion</h3>
+    <p>• Designed to mitigate key risks associated with breast expansion and ultimately reduce reoperation rates</p> 
+    <p>• Only tissue expander with an integral drain providing direct access to the periprosthetic space where fluid can accumulate</p>
+    <p>• Allows for diagnostic fluid sampling to enable a faster treatment response</p>
+	</div>
+</section>
+<section class="row">
+	<footer class="col-6 offset-3">
+		<h2>AlloX2 is designed to reduce the financial risks associated with breast reconstruction</h2>
+	</footer>
+</section>
 <?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
 @php the_content() @endphp
