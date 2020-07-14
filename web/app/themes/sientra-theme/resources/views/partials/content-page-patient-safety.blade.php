5c0fdd2a2a2d348b17320a3cbdaae1b6d3af184b
Mike <mikeh@ioprint.com>
2020-07-14 11:36:28 -0400

    pages

---
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-patient-safety.blade.php
@@ -0,0 +1,101 @@
+<section class="boxy-box text-center">
+  <div class="wrap px-0 pl-lg-3">  	
+  	
+  	<div class="col-12 col-lg-8 model text-left px-0 pl-lg-3">
+      <img src="@asset('images/recon-patient-safety.jpg')" alt="recon-patient-safety" width="1500" height="861" />
+  	</div>
+
+  	<div class="col-12 col-lg-5 message bg-white">
+      <div class="message-inner">
+        <img src="@asset('images/recon-patient-safety.svg')" alt="recon-patient-safety" />
+      </div>
+  	</div>
+  	  	
+  </div>
+</section>
+
+<section class="patient-safety row text-center">
+    <header class="col-12">
+      	<img src="@asset('images/patient-safety.svg')" alt="patient-safety" />
+    </header>
+  	<div class="col-12 col-md-10 offset-md-1 my-5">
+    			<h2 class="light">When it comes to patient safety, not all implants are created equal.<br>Sientra <span class="opus">OPUS</span> implants are clinically shown to have low complication rates.<sup>4</sup></h2>
+  	</div>
+
+    <div class="diagram col-12 mb-5">
+     	<img src="@asset('images/implant-diagram.png')" class="img-fluid d-md-none" alt="implant-diagram" width="1000" height="974" />
+     	<img src="@asset('images/implant-safety-diagram.svg')" class="img-fluid d-none d-md-block" alt="implant-safety-diagram" />
+    </div>
+
+    <div class="container">
+      <div class="row">    
+      <!-- 	 -->
+      	<article class="col-12 col-sm-4 mb-5">
+        	<div class="rounded-circle">
+        		<strong>Lowest</strong>
+      <p>capsular contracture rate<sup>*</sup></p>
+        	</div>
+      		<h3>Capsular Contracture</h3>
+      <p>A tightening/hardening 
+      of the scar tissue around 
+      the implant</p>
+      	</article>
+      <!-- 	 -->
+      	<article class="col-12 col-sm-4 mb-5">
+        	<div class="rounded-circle">
+        		<strong>Lowest</strong>
+        		<p>rupture rate<sup>*</sup></p>
+      		</div>
+      		<h3>Implant Rupture</h3>
+      <p>A hole or tear in the 
+      shell of the implant</p> 
+      
+      	</article>
+      <!-- 	 -->
+      	<article class="col-12 col-sm-4 mb-1 mb-md-5">
+        	<div class="rounded-circle">
+        		<strong>Lowest</strong>
+        		<p>reoperation rate<sup>*</sup></p>
+        	</div>
+      		<h3>Reoperation</h3>
+      <p>Any additional surgery performed to the breast after the first breast implantation</p> 
+      	</article>
+      <!-- 	 -->
+        <article class="col-12">
+        	    <p>*(primary reconstruction cohort)</p>
+        </article>
+      </div><!-- .row -->
+    </div><!-- .container -->
+  
+</section>
+<section class="warranty row">
+	<div class="h-line col-9 col-md-6 offset-md-3 text-center">
+		<img src="@asset('images/best-implant-warranty.svg')" alt="best-implant-warranty" />
+	</div>
+	<div class="col-3 col-md-2">
+		<img src="@asset('images/warranty.svg')" class="img-fluid" alt="warranty" />
+	</div>
+	<div class="col-12 col-md-8 offset-md-2 text-center my-5">
+		<h2 class="light">Because we believe in the safety and performance of our implants, we can offer the industry’s best warranty. </h2>
+	</div>
+</section>
+<section class="platinum20 container">
+	<div class="inner">
+  	<div class="col-12 text-center">
+  		<p>Our Sientra Platinum20™ Product Replacement and Limited Warranty Program provides the most comprehensive protection, longest length of coverage, most financial assistance, and least amount of warranty restrictions amongst all implant brands. </p>
+  		<p>Covers all primary and revision augmentation and reconstruction surgeries</p>
+  	</div>
+  	<div class="col-12 col-md-10 offset-md-1">
+  		<ul class="list-group"><li class="list-group-item"><span>Lifetime</span>FREE implant replacement for rupture</li>
+        <li class="list-group-item"><span>20 Year</span>Up to $5,000 for uncovered fees and costs due to implant rupture</li>
+        <li class="list-group-item"><span>20 Year</span>FREE implant replacement for complications of capsular contracture (Baker Grade III/IV), late forming seroma, and double capsule</li>
+        <li class="list-group-item"><span>2 Year</span>Up to $2,000 for uncovered fees and costs due to capsular contracture (Baker Grade III/IV), late forming seroma, and double capsule</li> 
+  		</ul>
+  		<p class="text-center small">For qualifying patients with late forming seroma, Sientra will cover the cost of complete testing, based on current best practices, for Breast Implant Associated-Anaplastic Large Cell Lymphoma (BIA-ALCL). </p>
+  		<p class="text-center">For complete details about the terms and conditions of the Sientra Platinum20 program and Sientra <span class="opus">OPUS</span> breast implants visit <span class="opus heavy">resources</span></p>
+  	</div>
+
+	</div>
+</section>
+<?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
+@php the_content() @endphp
