2fa1dde0c5f58ef65775e9082c79667bcc3d3df2
Mike <mikeh@ioprint.com>
2020-07-15 16:01:38 -0400

    pages

---
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-business-practices.blade.php
@@ -0,0 +1,92 @@
+<section class="boxy-box text-center">
+  <div class="wrap px-0 pl-lg-3">  	
+  	
+  	<div class="col-12 col-lg-8 model text-left px-0 pl-lg-3">
+      <img src="@asset('images/partners.jpg')" alt="partners" width="1500" height="861" />
+  	</div>
+
+  	<div class="col-12 col-lg-5 message bg-white">
+      <div class="message-inner">
+        <img src="@asset('images/recon-patient-safety.svg')" alt="recon-patient-safety" />
+      </div>
+  	</div>
+  	  	
+  </div>
+</section>
+
+<section class="lead container text-center">
+	<h1 class="light">Making it <span class="opus">easy</span> and <span class="opus">cost effective</span> to do business</h1>
+</section>
+
+<section class="features container">
+	<div class="row">
+		<article class="col-6">
+			<h2 class="book">Free <span class="opus">two-way</span> shipping</h2>
+			<div class="light">
+        <img src="@asset('images/icon-shipping.svg')" class="icon w-25" alt="icon-shipping" />
+        <ul>
+          <li>Shipping in both directions is on us. Consider it easy, worry-free ordering that won’t negatively impact your bottom line.</li>				
+        </ul>
+			</div>
+		</article>
+		
+		<article class="col-6">
+			<h2 class="book">No restocking penalty or fees</h2>
+			<div class="light">
+        <img src="@asset('images/icon-shipping.svg')" class="icon w-25" alt="icon-shipping" />
+        <ul>
+          <li>Receive full-credit for all returns up to 6 months. </li>				
+        </ul>
+      </div>
+		</article>
+	</div><!-- .row -->
+	
+	<div class="row">
+		<article class="col-6">
+			<h2 class="book">Provisional stock ordering</h2>
+			<div class="light">
+        <img src="@asset('images/icon-shipping.svg')" class="icon w-25" alt="icon-shipping" />
+        <ul>
+          <li>No billing for 35 days. Order up to 24 devices, use what you need, and return the rest. This ensures you are only billed for the products you use. </li>				
+        </ul>
+			</div>
+		</article>
+		
+		<article class="col-6">
+			<h2 class="book">Consignments</h2>
+			<div class="light">
+        <img src="@asset('images/icon-shipping.svg')" class="icon w-25" alt="icon-shipping" />
+        <ul>
+          <li>A generous consignment allotment helps ensure products are available when you need them. </li>				
+        </ul>
+      </div>
+		</article>
+	</div><!-- .row -->
+
+	<div class="row">
+		<article class="col-6">
+			<h2 class="book">Extended ordering hours</h2>
+			<div class="light">
+        <img src="@asset('images/icon-shipping.svg')" class="icon w-25" alt="icon-shipping" />
+        <ul>
+          <li>Our ordering lines are open longer to better accommodate your schedule</li>				
+        </ul>
+			</div>
+		</article>
+		
+		<article class="col-6">
+			<h2 class="book">Continuing education series</h2>
+			<div class="light">
+        <img src="@asset('images/icon-shipping.svg')" class="icon w-25" alt="icon-shipping" />
+        <ul>
+          <li>Learn about the latest surgical techniques and best practices from some of the country’s leading plastic and reconstructive surgeons.</li>				
+        </ul>
+			</div>
+		</article>
+	</div><!-- .row -->
+	
+
+</section>
+
+<?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
+@php the_content() @endphp
