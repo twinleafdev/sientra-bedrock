eb5172a4b40252a15804dbee2d62bae889a110a9
Mike <mikeh@ioprint.com>
2020-07-23 12:57:07 -0400

    pages

--- web/app/themes/sientra-theme/resources/views/partials/content-page-about.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-about.blade.php
@@ -1,5 +1,13 @@
 <section class="hero row">
-	<h1 class="image-h" style="background-image: url(@asset('images/about-hero-txt.svg'))">Advanced science. Exceptional Service. Unrivaled Safety.</h1>
+<!-- 	<h1 class="image-h" style="background-image: url(@asset('images/about-hero-txt.svg'))">Advanced science. Exceptional Service. Unrivaled Safety.</h1> -->
+
+
+  <video autoplay muted loop playsinline >
+      <source src="@asset('images/video/Sientra-About-Us-Video-Loop-Version-A.mp4')" type="video/mp4" >
+  </video>
+
+
+
 </section>
 <h2 class="text-center light my-5">A portfolio of medical aesthetic brands designed to make a difference in patients’ lives by enhancing their body image, growing their self-esteem, and restoring their confidence. Purposeful and differentiated technologies supported by strong clinical outcomes, that’s the Sientra difference. </h2>
 
