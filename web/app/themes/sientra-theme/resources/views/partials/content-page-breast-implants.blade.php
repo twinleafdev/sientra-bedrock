b4b38facb6a4821f0a5ca7e3d9bf84e54862d92e
jseverson4416 <jeff.severson@jtsdesign.com>
2020-08-11 17:35:09 -0400

    Began setting up the For US Surgeons Breast Implants page. Made changes to global and pages styles related to that template

--- web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implants.blade.php
+++ web/app/themes/sientra-theme/resources/views/partials/content-page-breast-implants.blade.php
@@ -2,12 +2,14 @@
   <div class="wrap px-0 pl-lg-3">  	
   	
   	<div class="col-12 col-lg-8 model text-left px-0 pl-lg-3">
-      <img src="@asset('images/implant-science.jpg')" alt="pittman" width="1200" height="691" />
+      	<?php $header_background = get_field('header_background'); ?>
+		<img src="<?php echo esc_url($header_background['url']); ?>" alt="<?php echo esc_url($header_background['alt']); ?>" width="1200" height="691" />
   	</div>
 
   	<div class="col-12 col-lg-5 message bg-white">
       <div class="message-inner">
-         <img src="@asset('images/breast-implants.svg')" class="mx-auto" alt="advanced-science" />
+		  <?php $header_title = get_field('header_title'); ?>
+         <img src="<?php echo esc_url($header_title['url']); ?>" class="mx-auto" alt="<?php echo esc_url($header_title['alt']); ?>" />
       </div>
   	</div>
   	  	
@@ -17,91 +19,120 @@
 <section class="desired-look text-center mx-auto">
   <header>
 <!--     <img src="@asset('images/implant-options-surg.svg')" class="img-fluid" alt="achieve your desired look" /> -->
-    <h3 class="image-h mx-auto" style="background-image:url(@asset('images/implant-options-surg.svg'));  height: 16vw; ">achieve your desired look</h3>
+    <h3 class="image-h mx-auto" style="background-image:url(<?php the_field('implant_options_image'); ?>);  height: 16vw; "><?php the_field('implant_options'); ?></h3>
   </header>
 	<div class="h2-wrap">
-		<h2>design features unique to all <span class="opus">OPUS</span> implants</h2>
+		<h2><?php the_field('design_features'); ?></h2>
 	</div>
 
 	<div class="gel-diagram container">
-      <img src="@asset('images/gel-diagram-mobile.jpg')" class="img-fluid my-5 d-md-none" alt="gel-diagram-mobile" width="1000" height="1333" />
-			<img src="@asset('images/gel-diagram.svg')" class="img-fluid my-5 d-none d-md-block" alt="gel-diagram" />
+		<?php $gel_diagram_mobile = get_field('gel_diagram_mobile'); ?>
+		<?php $gel_diagram = get_field('gel_diagram'); ?>
+      	<img src="<?php echo esc_url($gel_diagram_mobile['url']); ?>" class="img-fluid my-5 d-md-none" alt="<?php echo esc_url($gel_diagram_mobile['alt']); ?>" width="1000" height="1333" />
+		<img src="<?php echo esc_url($gel_diagram['url']); ?>" class="img-fluid my-5 d-none d-md-block" alt="<?php echo esc_url($gel_diagram['alt']); ?>" />
 	</div><!-- .container -->
 </section>
 <section class="surfaces row text-center">
 	<div class="mx-3 mb-3 wrap">
   	<div>
-    	<img src="@asset('images/smooth-surface.svg')" alt="smooth-surfaces" />
-  		<p>Soft yet durable, our smooth shell aids in the natural feel of our <span class="opus">OPUS</span> implants. The shell is designed to minimize rupture and allow the implant to move naturally with the natural breast tissue.</p>
+		<?php $smooth_surface_image = get_field('smooth_surface_image'); ?>
+    	<img src="<?php echo esc_url($smooth_surface_image['url']); ?>" alt="<?php echo esc_url($smooth_surface_image['alt']); ?>" />
+  		<?php the_field('smooth_surface'); ?>
   	</div>
 	</div>
 	<div class="mx-3 mb-3 wrap">
   	<div>
-	  	<img src="@asset('images/microtextured-surfaces.svg')" class="micro" alt="textured-surfaces" />
-<p>More rough and aggressive shell surfaces have been classified as macrotextured. Sientra’s proprietary texturing process results in a less aggressive, <span class="opus">microtextured surface</span>.<sup>3</sup></p>
+	  	<?php $microtextured_surface_image = get_field('microtextured_surface_image'); ?>
+		<img src="<?php echo esc_url($microtextured_surface_image['url']); ?>" class="micro" alt="<?php echo esc_url($microtextured_surface_image['alt']); ?>" />
+		<?php the_field('microtextured_surface'); ?>
   	</div>
 	</div>
 </section>
 
-<section class="surface-diagram container my-4"> 
-  <img src="@asset('images/surface-diagram-mobile.jpg')" class="img-fluid d-md-none" alt="surface-diagram-mobile" width="800" height="1368" />
-	<img src="@asset('images/surface-diagram.svg')" class="img-fluid d-none d-md-block" alt="surface-diagram" />
+<section class="surface-diagram container my-4">
+	<?php $surface_diagram_mobile = get_field('surface_diagram_mobile'); ?>
+	<?php $surface_diagram = get_field('surface_diagram'); ?>  
+	<img src="<?php echo esc_url($surface_diagram_mobile['url']); ?>" class="img-fluid d-md-none" alt="<?php echo esc_url($surface_diagram_mobile['alt']); ?>" width="800" height="1368" />
+	<img src="<?php echo esc_url($surface_diagram['url']); ?>" class="img-fluid d-none d-md-block" alt="<?php echo esc_url($surface_diagram['alt']); ?>" />
 	
 </section>
-<section class="gel-break-mobile row d-md-none"></section>
-<section class="gel-break row">
+<section class="gel-break-mobile row d-md-none" style="background-image:url(<?php the_field('gels_background'); ?>); ?>;"></section>
+<section class="gel-break row" style="background-image:url(<?php the_field('gels_background'); ?>); ?>;">
 	<div class="col-12 col-md-6 offset-md-6 text-center">
-		<h2 class="light">Two exclusive high-strength<br>cohesive gels – <span class="opus heavy">HSC</span> & <span class="opus heavy">HSC+</span></h2>
-<p class="light">Engineered for <span class="medium">optimal shape retention</span><br>and <span class="medium">fracture resistance</span> while<br>maintaining a <span class="medium">natural feel</span><sup>2</sup></p>
+		<h2 class="light"><?php the_field('gels_title'); ?></h2>
+<p class="light"><?php the_field('gels'); ?></p>
 	</div>
 </section>
 
 <section class="surfaces row text-center">
 	<div class="mx-3 mb-3 wrap">
   	<div>
-    	<img src="@asset('images/cohesive-gel-hsc.svg')" alt="cohesive-gel-hsc" />
-      <ul class="opus-list text-left">
-        <li>Soft feel mimics natural breast tissue</li>
-        <li>Impressive upper pole retention</li>
-        <li>More resistant to gel fracture than competitive gels<sup>2</sup></li>
-      </ul>
-      <p>Available on <i class="icon-luxe"></i> round implants</p>
+		<?php $hsc_image = get_field('hsc_image'); ?>
+    	<img src="<?php echo esc_url($hsc_image['url']); ?>" alt="<?php echo esc_url($hsc_image['alt']); ?>" />
+      <?php the_field('hsc'); ?>
       
   	</div>
 	</div>
 	<div class="mx-3 mb-3 wrap">
   	<div>
-	  	<img src="@asset('images/cohesive-gel-hsc-plus.svg')" alt="cohesive-gel-hsc-plus" />
-      <ul class="opus-list text-left">
-        <li>More cohesive than our HSC gel</li>
-        <li>Unmatched shape retention<sup>2</sup></li>
-        <li>Highest level of gel fracture resistance available<sup>2</sup></li>
-        <li>Designed to minimize wrinkling and rippling</li>
-      </ul>
-      <p>Available on <i class="icon-luxe"></i> round and <i class="icon-curve"></i> shaped implants</p>
+	  	<?php $hsc_plus_image = get_field('hsc_plus_image'); ?>
+    	<img src="<?php echo esc_url($hsc_plus_image['url']); ?>" alt="<?php echo esc_url($hsc_plus_image['alt']); ?>" />
+      <?php the_field('hsc_plus'); ?>
   	</div>
 	</div>
 </section>
 <section class="strength row my-5">
 	<header class="col-12">
-  	<h2 class="image-h" style="background-image: url(@asset('images/strength-softness.svg')); height: 14vw;">an optimal balance of Strength and softness</h2>
-		<h3 class="light text-center"><span class="opus">OPUS</span> implants offer the highest level of gel fracture resistance<br>available without the compromise of an overly firm implant<sup>2</sup></h3>
+  	<h2 class="image-h" style="background-image: url(<?php the_field('optimal_balance_image'); ?>); height: 14vw;"><?php the_field('optimal_balance_title'); ?></h2>
+		<h3 class="light text-center"><?php the_field('optimal_balance'); ?></h3>
 	</header>
 	<div class="col-md-6 px-5 my-2">
-		<img src="@asset('images/gel-graph.svg')" alt="gel-graph" />
+		<?php $gel_graph = get_field('gel_graph'); ?>
+		<img src="<?php echo esc_url($gel_graph['url']); ?>" alt="<?php echo esc_url($gel_graph['alt']); ?>" />
 		
 	</div>
 	<div class="softness col-md-5 my-2 align-self-center light text-center"> 
-  	<p><span class="opus">OPUS</span> HSC & HSC+ implants show minimum trade-off in softness in order to achieve greater gel strength<sup>2</sup></p>
+  	<?php the_field('gel_graph_text'); ?>
 	</div>
-	<small class="my-md-5 mx-5">*tests performed by an independent laboratory</small>
+	<small class="my-md-5 mx-5"><?php the_field('gel_graph_footnote'); ?></small>
 </section>
+<style>
+	.luxe {
+    	background-image: url(<?php the_field('luxe_background_mobile'); ?>);
+	}
+
+    @media (min-width: 768px) { 
+		.luxe {
+       		background-image: url(<?php the_field('luxe_background'); ?>);
+		}
+    }
+</style>
 <section class="luxe row mb-5">
- <header class="col-6 col-sm-4"> <img src="@asset('images/opus-luxe.png')" class="img-fluid" alt="opus-luxe" width="949" height="353" /></header>
+ <header class="col-6 col-sm-4">
+	 <?php $opus_luxe = get_field('opus_luxe'); ?>
+	 <img src="<?php echo esc_url($opus_luxe['url']); ?>" class="img-fluid" alt="<?php echo esc_url($opus_luxe['alt']); ?>" width="949" height="353" /></header>
  <div class="_250-round col-12 col-md-4 mb-2">
-  <img src="@asset('images/250-round.svg')" alt="250-round" />
+	 <?php $choices_250 = get_field('choices_250'); ?>
+  <img src="<?php echo esc_url($choices_250['url']); ?>" alt="<?php echo esc_url($choices_250['alt']); ?>" />
  </div>
  
+<style>
+	.projection-bg.low {
+        background-image: url(<?php the_field('luxe_projection_low'); ?>);
+    }
+    .projection-bg.mod {
+        background-image: url(<?php the_field('luxe_projection_moderate'); ?>);
+    }
+    .projection-bg.mod-plus {
+        background-image: url(<?php the_field('luxe_projection_moderate_plus'); ?>);
+    }
+    .projection-bg.high {
+        background-image: url(<?php the_field('luxe_projection_high'); ?>);
+    }
+    .projection-bg.xtra-high {
+        background-image: url(<?php the_field('luxe_projection_xtra_high'); ?>);
+    }	
+</style>
  
  <div class="d-none d-md-block luxe-projections text-center">
  	<div class="xtra-high projection">
@@ -121,19 +152,19 @@
  	</div> 
  	
  	<div class="xtra-high projection-bg">
- 		<p><span class="opus">Xtra high</span> projection  <span class="opus">|</span>  (275 cc - 510 cc)</p>
+ 		<p><?php the_field('luxe_projection_xtra_high_text'); ?></p>
  	</div>
  	<div class="high projection-bg">
- 		<p><span class="opus">high</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+ 		<p><?php the_field('luxe_projection_high_text'); ?></p>
  	</div>
  	<div class="mod-plus projection-bg">
- 		<p><span class="opus">moderate plus</span> projection  <span class="opus">|</span>  (175 cc - 695 cc)</p>
+ 		<p><?php the_field('luxe_projection_moderate_plus_text'); ?></p>
  	</div>
  	<div class="mod projection-bg">
- 		<p><span class="opus">moderate</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+ 		<p><?php the_field('luxe_projection_moderate_text'); ?></p>
  	</div>
  	<div class="low projection-bg">
- 		<p><span class="opus">low</span> projection  <span class="opus">|</span>  (160 cc - 700 cc)</p>
+ 		<p><?php the_field('luxe_projection_low_text'); ?></p>
  	</div>
  </div>
  
@@ -143,24 +174,24 @@
      <div class="swiper-container swiper-luxe-projection">
       <div class="swiper-wrapper">
         <div class="swiper-slide">
-          <img src="@asset('images/luxe-projection-low.png')" class="img-fluid" alt="luxe-projection-low" width="800" height="679" />
-          <p><span class="opus">low</span> projection  <span class="opus">|</span>  (160 cc - 700 cc)</p>
+          <img src="<?php the_field('luxe_projection_low'); ?>" class="img-fluid" alt="luxe-projection-low" width="800" height="679" />
+          <p><?php the_field('luxe_projection_low_text'); ?></p>
         </div>
         <div class="swiper-slide">
-          <img src="@asset('images/luxe-projection-moderate.png')" class="img-fluid" alt="luxe-projection-moderate" width="800" height="554" />
-          <p><span class="opus">moderate</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+          <img src="<?php the_field('luxe_projection_moderate'); ?>" class="img-fluid" alt="luxe-projection-moderate" width="800" height="554" />
+          <p><?php the_field('luxe_projection_moderate_text'); ?></p>
         </div>
         <div class="swiper-slide">
-          <img src="@asset('images/luxe-projection-moderate-plus.png')" class="img-fluid" alt="luxe-projection-moderate-plus" width="800" height="570" />
-          <p><span class="opus">moderate plus</span> projection  <span class="opus">|</span>  (175 cc - 695 cc)</p>
+          <img src="<?php the_field('luxe_projection_moderate_plus'); ?>" class="img-fluid" alt="luxe-projection-moderate-plus" width="800" height="570" />
+          <p><?php the_field('luxe_projection_moderate_plus_text'); ?></p>
         </div>
         <div class="swiper-slide">
-          <img src="@asset('images/luxe-projection-high.png')" class="img-fluid" alt="luxe-projection-high" width="800" height="592" />       
-          <p><span class="opus">high</span> projection  <span class="opus">|</span>  (190 cc - 700 cc)</p>
+          <img src="<?php the_field('luxe_projection_high'); ?>" class="img-fluid" alt="luxe-projection-high" width="800" height="592" />       
+          <p><?php the_field('luxe_projection_high_text'); ?></p>
         </div>
         <div class="swiper-slide">
-          <img src="@asset('images/luxe-projection-xrta-high.png')" class="img-fluid" alt="luxe-projection-xrta-high" width="800" height="506" />
-          <p><span class="opus">Xtra high</span> projection  <span class="opus">|</span>  (275 cc - 510 cc)</p>
+          <img src="<?php the_field('luxe_projection_xtra_high'); ?>" class="img-fluid" alt="luxe-projection-xrta-high" width="800" height="506" />
+          <p><?php the_field('luxe_projection_xtra_high_text'); ?></p>
         </div>
       </div><!-- .swiper-wrapper -->
      </div>
@@ -168,9 +199,23 @@
  
  </div>
 </section>
+
+<style>
+	.curve {
+    	background-image: url(<?php the_field('curve_background_mobile'); ?>);
+	}
+
+    @media (min-width: 768px) { 
+		.curve {
+       		background-image: url(<?php the_field('curve_background'); ?>);
+		}
+    }
+</style>
+
 <section class="curve row mb-5">
  <div class="_90-options col-12 col-md-4 mb-2 offset-md-5">
-  <img src="@asset('images/90-options.svg')" alt="90-options" />
+	 <?php $options_90 = get_field('options_90'); ?>
+  <img src="<?php echo esc_url($options_90['url']); ?>" alt="<?php echo esc_url($options_90['alt']); ?>" />
  </div>
  
   <div class="d-none d-md-flex col-md-3 curve-projections text-center">
